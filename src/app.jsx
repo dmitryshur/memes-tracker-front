631b760deb23f6c826825ebe57d3152d03661c61
Dmitry Shur <dimashur@gmail.com>
2018-03-04 11:08:17 -0500

    added home-container component

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomeContainer from './components/HomeContainer/HomeContainer.jsx';
+import './app.scss';
 
-const jsx = <div className="content">Content</div>;
+const jsx = (
+  <HomeContainer />
+);
 
 ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/components/HomeContainer/HomeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomeContainer.scss';
+
+const HomeContainer = (() => {
+  return (
+    <div className="home-container">
+      <div className="sliding-bg" />
+    </div>
+  );
+});
+
+export default HomeContainer;
