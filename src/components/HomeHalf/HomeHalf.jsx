16e66f297013b05bfa4d833485d386529fca3541
Dmitry Shur <dimashur@gmail.com>
2018-03-04 11:26:42 -0500

    Added HomeHalf component

---
+++ src/components/HomeHalf/HomeHalf.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './HomeHalf.scss';
+
+const HomeHalf = ((props) => {
+  return (
+    <div className={`home-half home-half-${props.side}`} />
+  );
+});
+
+HomeHalf.propTypes = {
+  side: PropTypes.string
+};
+
+HomeHalf.defaultProps = {
+  side: 'left'
+};
+
+export default HomeHalf;
