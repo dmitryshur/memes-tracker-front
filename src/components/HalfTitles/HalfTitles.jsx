9d24b31c1fa34a521a858b4863bc760927a1ac78
Dmitry Shur <dimashur@gmail.com>
2018-03-04 13:40:38 -0500

    Added HalfTitles component

---
+++ src/components/HalfTitles/HalfTitles.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HalfTitles.scss';
+
+const HalfTitles = () => {
+  return (
+    <div className="half-titles">
+      <div className="main-title">
+        memes<br />tracker
+      </div>
+      <div className="subtitle">
+        Your constant source of fun
+      </div>
+    </div>
+  );
+};
+
+export default HalfTitles;
--- src/components/HomeHalf/HomeHalf.jsx
+++ src/components/HomeHalf/HomeHalf.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './HomeHalf.scss';
+import HalfTitles from '../HalfTitles/HalfTitles.jsx';
 
-const HomeHalf = ((props) => {
+const HomeHalf = props => {
   return (
     <div className={`home-half home-half-${props.side}`}>
       <div className={`home-overlay home-overlay-${props.side}`} />
+      {props.side === 'left' && <HalfTitles />}
     </div>
   );
-});
+};
 
 HomeHalf.propTypes = {
-  side: PropTypes.string
+  side: PropTypes.string,
 };
 
 HomeHalf.defaultProps = {
-  side: 'left'
+  side: 'left',
 };
 
 export default HomeHalf;
