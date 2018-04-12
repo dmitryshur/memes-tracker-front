9f384c93607eaecfac7f99116dcaa84a20c5de24
Dmitry Shur <dimashur@gmail.com>
2018-04-12 16:28:27 -0400

    added transition for HalfTitles. changed tests

--- src/components/HalfTitles/HalfTitles.jsx
+++ src/components/HalfTitles/HalfTitles.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import './HalfTitles.scss';
 
-const HalfTitles = () => {
+const HalfTitles = (props) => {
+  const classList = props.chosen ? 'half-titles hidden' : 'half-titles';
+
   return (
-    <div className="half-titles">
+    <div className={classList}>
       <div className="main-title">
         memes<br />tracker
       </div>
@@ -12,6 +15,14 @@ const HalfTitles = () => {
       </div>
     </div>
   );
+};
+
+HalfTitles.propTypes = {
+  chosen: PropTypes.bool
+};
+
+HalfTitles.defaultProps = {
+  chosen: false
 };
 
 export default HalfTitles;
--- src/components/HalfTitles/HalfTitles.test.js
+++ src/components/HalfTitles/HalfTitles.test.js
@@ -6,3 +6,16 @@ test('testing HalfTitles component', () => {
   const wrapper = shallow(<HalfTitles />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('testing HalfTitles when newest memes were chosen', () => {
+  const wrapper = shallow(<HalfTitles chosen />);
+  expect(wrapper.find('.hidden').length).toBe(1);
+  expect(wrapper).toMatchSnapshot();
+});
+
+
+test('testing HalfTitles when liked memes were chosen', () => {
+  const wrapper = shallow(<HalfTitles chosen={false} />);
+  expect(wrapper.find('.hidden').length).toBe(0);
+  expect(wrapper).toMatchSnapshot();
+});
