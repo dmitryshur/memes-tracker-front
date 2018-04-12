3312f902936bcf278589eaa2faff9f47d2b03c54
Dmitry Shur <dimashur@gmail.com>
2018-04-12 17:13:38 -0400

    started working on navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './NavBar';
+
+const NavBar = () => {
+  return (
+    <div className='nav-bar' />
+  );
+}
+
+export default NavBar;
